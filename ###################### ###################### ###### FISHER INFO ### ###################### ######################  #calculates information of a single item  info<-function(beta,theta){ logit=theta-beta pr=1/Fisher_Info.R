f4c3bd8a43ba85a934156679ede3af220d279214
Beyza Doganay <beyzadoganay@gmail.com>
2024-12-14 15:55:52 +0300

    Create Fisher_Info.R

---
+++ ###################### ###################### ###### FISHER INFO ### ###################### ######################  #calculates information of a single item  info<-function(beta,theta){ logit=theta-beta pr=1/Fisher_Info.R
@@ -0,0 +1,15 @@
+
+######################
+######################
+###### FISHER INFO ###
+######################
+######################
+
+#calculates information of a single item
+
+info<-function(beta,theta){
+logit=theta-beta
+pr=1/(1+(exp(logit)^-1))
+inf=pr*(1-pr)
+return(inf)
+}
